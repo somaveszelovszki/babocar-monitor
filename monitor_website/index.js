632264f7aa1fa0e56ff5051a1e857500c2e2fa44
Soma Veszelovszki <soma.veszelovszki@gmail.com>
2019-08-28 09:38:00 +0200

    NodeJS testing

--- monitor_website/index.js
+++ monitor_website/index.js
@@ -1,25 +1,124 @@
-var http = require('http');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 var fs = require('fs');
-var SerialPort = require('serialport');
+const cheerio = require('cheerio')
 
-var port = new SerialPort('/dev/ttyAMA0', {
-   baudRate: 57600,
-   dataBits: 8,
-   parity: 'none',
-   stopBits: 1,
-   flowControl: false
+app.get('/', function(req, res) {
+    //res.sendfile('index.html');
+  
+    fs.readFile('index.html', function(err, data) {
+      if (err) {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        return res.end("404 Not Found");
+      }
+      res.writeHead(200, {'Content-Type': 'text/html'});
+  
+      const $ = cheerio.load(data);
+      
+      console.log("Index");
+      $('p').text('Hello there!');
+      
+      $('table#debug-params').append(
+          '<tr>' +
+              '<td onclick="debugParamClickHandler(this)">Jill</td>' +
+              '<td>Smith</td>' +
+          '</tr>'
+      );
+      
+      res.write($.html());
+      return res.end();
+    }); 
+  
 });
 
-http.createServer(index).listen(8080);
+io.on('connection', function(socket) {
+   console.log('A user connected');
+  
+   setTimeout(function() {
+      socket.emit('testerEvent', { desc: 'A custom event named testerEvent!'});
+   }, 4000);
 
-function index (req, res) {
-  fs.readFile('index.html', function(err, data) {
-    if (err) {
-      res.writeHead(404, {'Content-Type': 'text/html'});
-      return res.end("404 Not Found");
-    }
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
-}
+   socket.on('clientEvent', function (data) {
+      console.log(data);
+   });
+  
+   socket.on('disconnect', function () {
+      console.log('A user disconnected');
+   });
+});
+
+//var SerialPort = require('serialport');
+//var port = new SerialPort('/dev/ttyAMA0', {
+//   baudRate: 115200,
+//   dataBits: 8,
+//   parity: 'none',
+//   stopBits: 1,
+//   flowControl: false
+//});
+// 
+//port.on('open', function() {
+//  port.write('main screen turn on', function(err) {
+//    if (err) {
+//      return console.log('Error on write: ', err.message);
+//    }
+//    console.log('message written');
+//  });
+//});
+// 
+//// open errors will be emitted as an error event 
+//port.on('error', function(err) {
+//  console.log('Error: ', err.message);
+//});
+
+http.listen(8080, function() {
+   console.log('listening on *:8080');
+});
+
+//http.createServer(index).listen(8080);
+//
+//function index (req, res) {
+//  if (req.method === 'POST') {
+//    
+//    let body = '';
+//    req.on('data', chunk => {
+//        body += chunk.toString();
+//    });
+//    req.on('end', () => {
+//        console.log(body);
+//        res.end('ok');
+//    });
+//    
+//  } else {
+//    
+//    fs.readFile('index.html', function(err, data) {
+//      if (err) {
+//        res.writeHead(404, {'Content-Type': 'text/html'});
+//        return res.end("404 Not Found");
+//      }
+//      res.writeHead(200, {'Content-Type': 'text/html'});
+//  
+//      const $ = cheerio.load(data);
+//      
+//      console.log("Index");
+//      $('p').text('Hello there!');
+//      
+//      $('table#debug-params').append(
+//          '<tr>' +
+//              '<td onclick="myFunction(this)">Jill</td>' +
+//              '<td>Smith</td>' +
+//          '</tr>'
+//      );
+//      
+//      res.write($.html());
+//      return res.end();
+//    });  
+//    
+//  }
+//}
+//
+//function myFunction () {
+//    console.log("clicked");
+//}
+
+//=> div appended to body
