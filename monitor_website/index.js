fb9d636bb64b3ca0b007b30212f39a78a3d9c7a2
Soma Veszelovszki <soma.veszelovszki@gmail.com>
2019-08-31 20:28:10 +0200

    First iteration of debug parameter view implemented, tested with example JSON

--- monitor_website/index.js
+++ monitor_website/index.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var fs = require('fs');
-const cheerio = require('cheerio')
+const cheerio = require('cheerio');
 
 app.get('/', function(req, res) {
     //res.sendfile('index.html');
@@ -32,20 +32,23 @@ app.get('/', function(req, res) {
   
 });
 
-io.on('connection', function(socket) {
-   console.log('A user connected');
+var socket;
+io.on('connection', function(socket_) {
+    socket = socket_;
+    console.log('A user connected');
+    sendDebugParams(JSON.parse('{"car":{"pose":{"pos":{"X":100.0000,"Y":200.0000},"angle":5.0000},"speed":1.5000},"useSafetySignal":false,"motorEnabled":true}'));
   
-   setTimeout(function() {
-      socket.emit('testerEvent', { desc: 'A custom event named testerEvent!'});
-   }, 4000);
+    setTimeout(function() {
+        socket.emit('testerEvent', { desc: 'A custom event named testerEvent!'});
+    }, 4000);
 
-   socket.on('clientEvent', function (data) {
-      console.log(data);
-   });
+    socket.on('clientEvent', function (data) {
+        console.log(data);
+    });
   
-   socket.on('disconnect', function () {
-      console.log('A user disconnected');
-   });
+    socket.on('disconnect', function () {
+        console.log('A user disconnected');
+    });
 });
 
 var SerialPort = require('serialport');
@@ -66,13 +69,45 @@ port.on('open', function() {
   });
 });
 
+function printDebugParamValues(parent, obj) {
+  let html = '';
+  html += '<table>';
+  if (typeof obj == 'object') {
+    for (let key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        let id = (parent == '' ? '' : parent + '-') + key;
+        html += '<tr><td valign="top" id="' + id + '"><label>' + key + '</label></td>' +
+                '<td><div class="param-value">';
+        html += printDebugParamValues(id, obj[key]);
+        html += '</div></td></tr>';
+      }
+    }
+  } else {
+    if (typeof obj == 'boolean') {
+      html += '<input type="checkbox"' + (obj ? ' checked' : '') + '/>';
+    } else {
+      html += '<input type="text" value="' + obj + '"/>';      
+    }
+  }
+
+  html += '</table>';
+
+  return html;
+}
+
+function sendDebugParams(obj) {
+  var html = printDebugParamValues('', obj);
+  console.log(html);
+  socket.emit('debugParamValues', html);
+}
+
 var uartRecv = '';
 port.on('data', function(data) {
   uartRecv += data;
-  if (data.indexOf('d') != -1) {
+  if (data.indexOf('$') != -1) {
     console.log('Data received throguh UART: ', uartRecv);
-    uartRecv = '';
-  }
+    sendDebugParams(JSON.parse(uartRecv));
+    uartRecv = '';  }
 });
  
 // open errors will be emitted as an error event 
