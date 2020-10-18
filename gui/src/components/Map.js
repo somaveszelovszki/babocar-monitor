31bb0f78c3ab21f5a682de7c5b46112b26830f1f
Bence Sebők <bence.sebok.personal@gmail.com>
2020-10-18 13:12:55 +0200

    Refactor Map component

--- gui/src/components/Map.js
+++ gui/src/components/Map.js
@@ -1,11 +1,7 @@
 import React from "react";
-import mapIcon from '../../resources/img/map.png'
 
 const lineColor = 'black';
 const lineWidth = '2';
-const circleWidth = '1';
-const circleColor = 'black';
-const circleColorActive = 'red';
 const canvasSize = 640
 const canvasWidth = canvasSize 
 const canvasHeight = canvasSize 
@@ -17,30 +13,28 @@ class Map extends React.Component {
         this.state = {
             coordinates: this.props.coordinates
         }
-	this.realityToMap = this.realityToMap.bind(this)
+	      this.realityToMap = this.realityToMap.bind(this)
       }
+
     componentDidMount() {
         const canvas = this.refs.canvas
         const ctx = canvas.getContext("2d")
         ctx.strokeStyle = lineColor;
         ctx.lineWidth = lineWidth;
-        const coordinates = this.state.coordinates
-        //console.log('Map componentDidMount coordinates:', this.props.coordinates);
     }
-    componentWillReceiveProps()
-    {
-        //console.log('Map componentWillReceiveProps coordinates:', this.props.coordinates);
+
+    componentWillReceiveProps() {
         this.setState({ coordinates: this.props.coordinates})
     }
+
     realityToMap(coordinateCm, axis) {
-		return (axis === 'y' ? -coordinateCm : coordinateCm) * canvasSize / mapSizeCm + canvasSize/2
-	}
+  		return (axis === 'y' ? -coordinateCm : coordinateCm) * canvasSize / mapSizeCm + canvasSize/2
+    }
+    
     componentDidUpdate(prevProps)
     {
-        //console.log('Map componentDidUpdate coordinates:', this.props.coordinates);
         if(prevProps.coordinates.length != this.props.coordinates && this.props.coordinates.length >= 2)
         {
-          //console.log('componentDidUpdate new coordinate from', this.props.coordinates[this.props.coordinates.length-2], ' to ', this.props.coordinates[this.props.coordinates.length-1]);
           const canvas = this.refs.canvas
           const ctx = canvas.getContext("2d")
           // Line
@@ -55,23 +49,15 @@ class Map extends React.Component {
             ctx.font = "10px Arial";
             ctx.fillText(Math.round(coordinates[this.props.coordinates.length-1]['junction']), this.realityToMap(coordinates[this.props.coordinates.length-1].x+5, 'x'), this.realityToMap(coordinates[this.props.coordinates.length-1].y, 'y'));
           }
-          /*
-          for(var i = 0; i < coordinates.length - 1; i++)
-          {
-              ctx.moveTo(coordinates[i].x + 50, coordinates[i].y + 50);
-              ctx.lineTo(coordinates[i+1].x + 50, coordinates[i+1].y + 50);
-              ctx.stroke();
-          }
-          */
         }
     }
     render() {
-        return(
+        return (
           <div>
-		<div style = {{textAlign: "center"}}><h1>Map</h1><img src = {mapIcon} alt = 'Map icon' /></div>
             <canvas ref="canvas" width={canvasWidth} height={canvasHeight} />
           </div>
         )
       }
     }
+
 export default Map
