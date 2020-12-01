9a08f3ee81753369cffb1e8f9ee801b1869fc14d
Bence Sebők <bence.sebok.personal@gmail.com>
2020-12-01 21:54:03 +0100

    Add custom field feature to RefactoredForm

--- gui/src/components/RefactoredForm.js
+++ gui/src/components/RefactoredForm.js
@@ -8,13 +8,16 @@ export default class RefactoredForm extends React.Component {
       this.state = {
         focusedItem: null,
         formData: {},
-        serialData: null
+        serialData: null,
+        customFieldName: '',
+        customFieldValue: ''
       }
       this.handleSubmit = this.handleSubmit.bind(this)
       this.onInputChange = this.onInputChange.bind(this)
       this.handleClick = this.handleClick.bind(this)
       this.handleEnter = this.handleEnter.bind(this)
       this.updateFormData = this.updateFormData.bind(this)
+      this.handleCustomFieldChange = this.handleCustomFieldChange.bind(this)
     }
   
     onInputChange = (input) => {
@@ -72,6 +75,7 @@ export default class RefactoredForm extends React.Component {
   
     handleSubmit() {
       var localData = this.state.formData
+      const { customFieldName, customFieldValue } = this.state
       for (let [key, value] of Object.entries(this.state.formData)) {
         if(typeof(value) === 'number') {
           localData[key] = parseFloat(value).toFixed(4)
@@ -82,9 +86,27 @@ export default class RefactoredForm extends React.Component {
           }
         }
       }
+      // Add custom field to serial data if it is not empty
+      if(customFieldName !== '' && customFieldValue !== '') {
+        localData[customFieldName] = customFieldValue
+      }
       console.log('localData', localData)
       this.props.socket.emit('dataFromClient', '[P]' + JSON.stringify(localData) + '$')
     }
+
+    
+    // This function updates the custom field in the state management
+    handleCustomFieldChange(event) {
+      const target = event.target;
+      const { value, name } = target;
+      //const name = target.name;
+      console.log('handleCustomFieldChange', event, target, name, value);
+
+      this.setState({
+        [name]: value
+      });
+    }
+
     render() {
       var renderedElements = null
       const { focusedItem, serialData, formData } = this.state
@@ -114,7 +136,29 @@ export default class RefactoredForm extends React.Component {
                 <tbody>
                     {renderedElements}
                     <tr>
-                      <td colSpan="2"><Button variant="info" type="submit" onClick={this.handleSubmit}>Send to serial port</Button></td>
+                      <td>
+                        <input
+                          type="text"
+                          id="customFieldName"
+                          name="customFieldName"
+                          value = {this.state.customFieldName}
+                          onChange = {this.handleCustomFieldChange}
+                          onKeyPress={(e) => this.handleEnter(e)}
+                          placeholder = "Custom field name"
+                        />
+                      </td>
+                      <td>
+                        <input
+                          type="text"
+                          id="customFieldValue"
+                          name="customFieldValue"
+                          value = {this.state.customFieldValue}
+                          onChange = {this.handleCustomFieldChange}
+                          onKeyPress={(e) => this.handleEnter(e)}
+                          placeholder = "Custom field value"
+                        />
+                      </td>
+                    </tr>
                     </tr>
                 </tbody>
               </Table>
