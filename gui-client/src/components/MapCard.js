8e12ba9b3c2320097778d4b81a9166cae45146e8
Soma Veszelovszki <soma.veszelovszki@gmail.com>
2023-05-21 19:05:42 +0200

    Initial version of map added, car simulation moves car in a serpent

--- gui-client/src/components/MapCard.js
+++ gui-client/src/components/MapCard.js
@@ -2,11 +2,50 @@ import React from 'react';
 import { Card } from 'react-bootstrap';
 
 export default function MapCard({ car }) {
+    const [positions, setPositions] = React.useState([car.pos_m]);
+
+    if (positions.at(-1) !== car.pos_m) {
+        setPositions([...positions, car.pos_m]);
+    }
+
+    const canvasRef = React.useRef(null);
+
+    React.useEffect(() => {
+        const context = canvasRef.current.getContext('2d');
+        context.fillStyle = '#FFFFFF';
+        context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+
+        const mapSize = { x: 10, y: 10 };
+
+        function convert(pos) {
+            return {
+                x: context.canvas.width / 2 + pos.x / mapSize.x * context.canvas.width,
+                y: context.canvas.height / 2 - pos.y / mapSize.y * context.canvas.height
+            };
+        }
+
+        let prev = convert(positions[0]);
+        positions.forEach(pos => {
+            const current = convert(pos);
+            context.beginPath();
+            context.moveTo(prev.x, prev.y);
+            context.lineTo(current.x, current.y);
+            context.strokeStyle = 'red';
+            context.lineWidth = 1;
+            context.stroke();
+
+            prev = current;
+        });
+
+    }, [positions]);
+
     return (
         <Card>
             <Card.Body>
                 <Card.Title>Map</Card.Title>
-                {/* TODO */}
+                <div className='square'>
+                    <canvas ref={canvasRef} style={{ position: 'absolute', left: 0, top: 0, width: '100%', height: '100%' }} />
+                </div>
             </Card.Body>
         </Card>
     )
--- test-connector/src/index.js
+++ test-connector/src/index.js
@@ -22,10 +22,13 @@ socket.on('feed', (json) => {
     }
 });
 
+const SIMULATION_SPEED_MPS = 1;
+const SIMULATION_INTERVAL_MS = 100;
+
 let car = {
-    pos_m: { x: 0, y: 0 },
+    pos_m: { x: 1, y: 0 },
     angle_rad: 0,
-    speed_mps: 0
+    speed_mps: SIMULATION_SPEED_MPS
 };
 
 let logIndex = 0;
@@ -37,16 +40,35 @@ let params = {
 };
 
 setInterval(() => {
-    car.pos_m.x += 0.5;
-    car.pos_m.y += 1.0;
-    car.angle_rad += Math.PI / 10;
-    car.speed_mps += 0.1;
+    broadcastCar();
+}, SIMULATION_INTERVAL_MS);
+
+setInterval(() => {
+    broadcastLog();
+    broadcastParams();
+}, 1000);
+
+function broadcastCar() {
+    const radius = Math.sqrt(car.pos_m.x * car.pos_m.x + car.pos_m.y * car.pos_m.y);
+    const distance = SIMULATION_SPEED_MPS * (SIMULATION_INTERVAL_MS / 1000);
+    const d_angle = distance / radius;
+    const newRadius = radius * 1.001;
+
+    car.angle_rad += d_angle;
+    if (car.angle_rad > 2 * Math.PI) {
+        car.angle_rad -= 2 * Math.PI;
+    }
+
+    car.pos_m.x = newRadius * Math.cos(car.angle_rad);
+    car.pos_m.y = newRadius * Math.sin(car.angle_rad);
 
     socket.emit('send', JSON.stringify({
         channel: 'car',
         car: car
     }));
+}
 
+function broadcastLog() {
     logIndex += 1;
 
     let getLogLevel = () => {
@@ -66,14 +88,16 @@ setInterval(() => {
             text: `Log message from server #${logIndex}`
         }
     }));
+}
 
+function broadcastParams() {
     params.motorCtrl_P += 0.1;
 
     socket.emit('send', JSON.stringify({
         channel: 'params',
         params: params
     }));
-}, 1000);
+}
 
 function updateParams(paramsIn) {
     Object.keys(paramsIn).forEach(key => {
