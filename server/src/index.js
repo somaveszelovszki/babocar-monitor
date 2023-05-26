114cfc8f1cf8ee3319a1c6f35c9b210b5b836bb3
Soma Veszelovszki <soma.veszelovszki@gmail.com>
2023-05-26 10:06:46 +0200

    Supporting wildcard channel subscription

--- server/src/index.js
+++ server/src/index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 app.use(cors());
-var server = http.Server(app);
+let server = http.Server(app);
 
 const io = socketIO(server, {
     cors: {
@@ -16,22 +16,46 @@ const io = socketIO(server, {
     }
 });
 
+io.wildCardClients = new Map();
+
 io.on('connection', (client) => {
     console.log(`${client.id} connected`);
 
     client.on('subscribe', function (channel) {
-        console.log(`${client.id} subscribed to '${channel}'`);
-        client.join(channel);
+        if (channel == '*') {
+            console.log(`${client.id} subscribed to all channels`);
+            io.wildCardClients.set(client.id, { socket: client, subscriptions: new Set() });
+        } else {
+            console.log(`${client.id} subscribed to channel: '${channel}'`);
+            client.join(channel);
+        }
     })
 
     client.on('unsubscribe', function (channel) {
-        console.log(`${client.id} unsubscribed from '${channel}'`);
-        client.leave(channel);
+        if (channel == '*') {
+            console.log(`${client.id} unsubscribed from all channels`);
+            io.wildCardClients.delete(client.id);
+            io.rooms.forEach((c) => client.leave(c));
+        } else {
+            console.log(`${client.id} unsubscribed from channel: '${channel}'`);
+            client.leave(channel);
+        }
     })
 
     client.on('send', function (msg) {
+        const channel = JSON.parse(msg).channel;
+
+        io.wildCardClients
+            .forEach((c, id) => {
+                if (!c.subscriptions.has(channel)) {
+                    console.log(`${id} subscribed to channel: '${channel}'`);
+                    c.subscriptions.add(channel);
+                    c.socket.join(channel);
+                }
+            });
+
         console.log(`Broadcasting message: ${msg}`);
-        client.broadcast.to(JSON.parse(msg).channel).emit('feed', msg);
+        client.broadcast.to(channel).emit('feed', msg);
     });
 
     client.on('disconnect', () => {
